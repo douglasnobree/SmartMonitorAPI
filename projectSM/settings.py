41eb0815e7415f68053f5bfbc7dde8e54d9c603b
Douglas Nobre <douglasclaudino543@gmail.com>
2024-10-17 14:49:09 -0300

    SETTINGS

--- projectSM/settings.py
+++ projectSM/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -151,7 +151,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Diretório onde os arquivos estáticos serão coletados
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # ou o caminho que você preferir
+
+# Diretórios adicionais onde o Django deve procurar arquivos estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # ou outros diretórios que você esteja usando
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
